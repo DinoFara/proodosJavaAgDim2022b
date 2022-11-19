d200181619d270c5045238d1a00c0355c5a90974
Spyros <Spyros@Markos.mshome.net>
2022-11-19 16:09:17 +0200

    Anevasma Proodou

---
+++ src/main/java/gr/iek/proodos9025CoreCode/Player.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gr.iek.proodos9025CoreCode;
+
+/**
+ *
+ * @author Spyros
+ */
+public class Player {
+    
+    String firstName; // ιδιοτητα για το μικρο ονομα
+    String lastName; // ιδιοτηταα για το επιθετο
+    Date birtDate;      // ιδιοτητα για την ημερομηνια γεννησης
+    
+    
+    public String getFirstName(){
+        
+    } 
+    
+    public String getLastName(){
+        
+    }
+    
+    public Date getBirthDate(){
+        
+    }
+    
+    public void setFirstName(){
+        
+    }
+    
+    public void setLastName(){
+        
+    }
+    
+    public void printInfo(){
+        
+    }
+}
---
+++ src/main/java/gr/iek/proodos9025CoreCode/Proodos9025AgDim2022B.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package gr.iek.proodos9025CoreCode;
+
+/**
+ *
+ * @author Spyros
+ */
+public class Proodos9025AgDim2022B {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
